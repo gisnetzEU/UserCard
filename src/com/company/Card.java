d49a0129932f211bf9ec9658bc5ed695f05394d0
Francesc <ffiguerass@gmail.com>
2022-04-06 10:16:13 +0200

    first commit

---
+++ src/com/company/Card.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Card {
+
+    int number;
+    double amount;
+    String type;
+    int pin;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getPin() {
+        return pin;
+    }
+
+    public void setPin(int pin) {
+        this.pin = pin;
+    }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        ArrayList<user> users = new ArrayList();
+        Scanner reader= new Scanner(Syste.in);
+
+        while(true){
+
+            //print mini-menu with 4 options
+            //call-operation to create new user
+            //call-operation to change pin
+            //call-operation to make transfer
+            //call-operation to deposit, to charge, to add some money to the card
+        }
+
+        public static void createUser(){
+
+        }
+
+        public static void changePin(){
+
+        }
+
+        public static void transfer(){
+
+        }
+
+        public static void deposit(){
+
+        }
+
+
+    }
+}
